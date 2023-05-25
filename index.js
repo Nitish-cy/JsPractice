4eb18bbf81ba50366ad7ceb5051236028eadb3d8
Nitish.mishra1 <Nitish.mishra1@RBLLNHO0424>
2023-05-25 07:58:21 +0530

    Operators in JS

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 console.log("Hello World");
 
-let age=5;
-console.log(age);
+// let age=5;
+// console.log(age);
 
-const age1=5;
-console.log(age1);
+// const age1=5;
+// console.log(age1);
+
+// const person={
+//     'name':'Nitish',
+//     'Last Name':'Mishra',
+//     'Address':'Ramgarh'
+// }
+// console.log(person.name);
+
+// const arr=['Nitish',1,'B'];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
