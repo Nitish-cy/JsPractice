fd01010ce2843d131342516d61d7104148acb44d
Nitish.mishra1 <Nitish.mishra1@RBLLNHO0424>
2023-06-17 11:32:39 +0530

    Call Apply and Bind  in js

---
+++ CallAppllyandBindMethod.js
@@ -0,0 +1,23 @@
+let obj={
+    name:'Nitish',
+    lastName:'Mishra',
+    address:'Ramgarh',
+    getFullName:function(dis,state){
+        console.log('Full Name is : ' + this.name +' ' + this .lastName + ' dist is '+ dis + ' state is ' + state)
+    }
+}
+
+let obj2={
+    name:"Shubham",
+    lastName:"Mishra",
+}
+//function borrowing
+// obj.getFullName.call(obj2);
+// obj.getFullName.apply(obj2);
+
+obj.getFullName.call(obj2,'Sultanpur','Uttar Pradesh');
+obj.getFullName.apply(obj2,['Sultanpur','Uttar Pradesh']);
+
+// obj.getFullName();
+//ther is no diff between call and apply -- but diff is if we want to pass arguments then we will pass - comma seperated but in apply we 
+//can pass as an array
